5650913e9612c10a57c3cf150417539effedca2d
Naira98 <nairamm99@gmail.com>
2025-05-30 22:30:43 +0300

    work in progress

--- src/services/exam.js
+++ src/services/exam.js
@@ -1,5 +1,13 @@
 import { countDownTimer } from "../utils/countdownTimer.js";
 
+const progressBar = document.querySelector(".progressBar");
+enableProgressBar();
+function enableProgressBar() {
+  progressBar.setAttribute("role", "progressbar");
+  progressBar.setAttribute("aria-valuenow", 0);
+  progressBar.setAttribute("aria-live", "polite");
+}
+
 let timer = 1.5 * 60 * 1000;
 
-countDownTimer(timer)
+countDownTimer(timer);
