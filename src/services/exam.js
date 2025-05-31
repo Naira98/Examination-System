c48b8dfa04a5833cca5164128b0673861760ed1c
Naira98 <nairamm99@gmail.com>
2025-05-31 14:44:02 +0300

    circular timer

--- src/services/exam.js
+++ src/services/exam.js
@@ -1,13 +1,5 @@
 import { countDownTimer } from "../utils/countdownTimer.js";
 
-const progressBar = document.querySelector(".progressBar");
-enableProgressBar();
-function enableProgressBar() {
-  progressBar.setAttribute("role", "progressbar");
-  progressBar.setAttribute("aria-valuenow", 0);
-  progressBar.setAttribute("aria-live", "polite");
-}
+let timer = 1.5 * 60;
 
-let timer = 1.5 * 60 * 1000;
-
-countDownTimer(timer);
+countDownTimer(timer, 30);
--- src/utils/countdownTimer.js
+++ src/utils/countdownTimer.js
@@ -1,53 +1,37 @@
-export function countDownTimer (timer) {
-    let x = setInterval(function () {
-  if (timer <= 0) {
-    clearInterval(x);
-    return;
-  }
+const minutesClock = document.querySelector(".minutes");
+const secondsClock = document.querySelector(".seconds");
+const timeElem = document.querySelector(".timer");
 
-  let days = Math.floor(timer / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((timer % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((timer % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((timer % (1000 * 60)) / 1000);
+export function countDownTimer(time, almostDone) {
+  const originalTime = time;
 
-  timer -= 1000;
+  timer();
+  let x = setInterval(timer, 1000);
 
-  if (days < 10) {
-    days = `0${days}`;
-  }
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-
-  // Get elements by class name
-  let countdownElements = document.getElementsByClassName("countdown-element");
-
-  // Loop through the elements and update their content
-  for (let i = 0; i < countdownElements.length; i++) {
-    let className = countdownElements[i].classList[1]; // Get the second class name
-    // console.log(className)
-    switch (className) {
-      case "days":
-        countdownElements[i].innerHTML = days;
-        break;
-      case "hours":
-        countdownElements[i].innerHTML = hours;
-        break;
-      case "minutes":
-        countdownElements[i].innerHTML = minutes;
-        break;
-      case "seconds":
-        countdownElements[i].innerHTML = seconds;
-        break;
-      default:
-        break;
+  function timer() {
+    if (time < 0) {
+      clearInterval(x);
+      return;
     }
+
+    if (time <= almostDone) {
+      timeElem.style.setProperty("--color-brand", "var(--color-error)");
+    }
+
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    time -= 1;
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+
+    minutesClock.innerText = minutes;
+    secondsClock.innerText = seconds;
+    timeElem.style.setProperty("--angle", `${(360 * time) / originalTime}deg`);
   }
-}, 1000);
 }
