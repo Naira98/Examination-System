ba7f2fd339261b0852d7674ff02d78e8f8a26da5
Naira98 <nairamm99@gmail.com>
2025-06-01 18:25:58 +0300

    erase and mark buttons

--- src/services/exam.js
+++ src/services/exam.js
@@ -8,56 +8,81 @@ const previousBtn = document.querySelector("#previous");
 const nextBtn = document.querySelector("#next");
 const openedQuestion = document.querySelector("#openedQuestion");
 const numberOfQuestions = document.querySelector("#numberOfQuestions");
+const eraseBtn = document.querySelector("#eraseAnswer");
+const markBtn = document.querySelector("#markQuestion");
 
 let timeInSeconds = 1.5 * 60;
 let currentQuestionIndex = 0;
 
-// countDownTimer(timeInSeconds, 30);
+countDownTimer(timeInSeconds, 30);
 
 /* Questions */
 let res = await fetch("/src/data/questions.json");
 let questions = await res.json();
-const answersMap = new Map();
+const choosenAnswers = new Map();
+const markedQuestions = new Set();
 
 questions = shuffleQuestions(questions);
 showQuestion(currentQuestionIndex);
 
-enableDisableBtns();
 numberOfQuestions.innerText = `/ ${questions.length}`;
+enableDisablePrevNextBtns();
 
-previousBtn.addEventListener("click", () => {
-  currentQuestionIndex -= 1;
-  showQuestion(currentQuestionIndex);
-  enableDisableBtns();
-  openedQuestion.innerText = currentQuestionIndex + 1;
-});
-nextBtn.addEventListener("click", () => {
-  currentQuestionIndex += 1;
-  showQuestion(currentQuestionIndex);
-  enableDisableBtns();
-  openedQuestion.innerText = currentQuestionIndex + 1;
+let prevNextBtn = [
+  {
+    btn: previousBtn,
+    operation: () => {
+      currentQuestionIndex -= 1;
+    },
+  },
+  {
+    btn: nextBtn,
+    operation: () => {
+      currentQuestionIndex += 1;
+    },
+  },
+];
+
+for (let { btn, operation } of prevNextBtn) {
+  btn.addEventListener("click", () => {
+    operation();
+    showQuestion(currentQuestionIndex);
+    enableDisablePrevNextBtns();
+    openedQuestion.innerText = currentQuestionIndex + 1;
+  });
+}
+
+eraseBtn.addEventListener("click", () => {
+  choosenAnswers.delete(currentQuestionIndex);
+  removeActiveAnswer();
 });
 
+markBtn.addEventListener("click", () => {
+  markedQuestions.add(currentQuestionIndex);
+  enableDisableMarkBtn();
+});
+
+/* ------- Question, Choices, EventListener to answer question ------- */
 function showQuestion(questionIndex) {
-  questionNo.innerText = `${questionIndex + 1}.`;
-  question.innerText = questions[questionIndex].question;
+
+  questionNo.textContent = `${questionIndex + 1}.`;
+  question.textContent = questions[questionIndex].question;
   choices.innerHTML = "";
 
   for (let answerIndex in questions[questionIndex].answers) {
     let div = document.createElement("div");
 
     div.classList.add("answer");
-    if (answersMap.get(questionIndex)) {
+    if (choosenAnswers.get(questionIndex) == answerIndex) {
       div.classList.add("activeAnswer");
     }
 
     div.textContent = questions[questionIndex].answers[answerIndex].text;
 
     div.addEventListener("click", () => {
-      const activeAnswers = document.querySelectorAll(".activeAnswer");
-      activeAnswers.forEach((e) => e.classList.remove("activeAnswer"));
-
-      answersMap.set(questionIndex, answerIndex);
+      removeActiveAnswer();
+      choosenAnswers.set(questionIndex, answerIndex);
+      eraseBtn.disabled = false;
       div.classList.add("activeAnswer");
     });
 
@@ -65,7 +90,16 @@ function showQuestion(questionIndex) {
   }
 }
 
-function enableDisableBtns() {
+function enableDisablePrevNextBtns() {
   previousBtn.disabled = currentQuestionIndex === 0;
   nextBtn.disabled = currentQuestionIndex === questions.length - 1;
+}
+
+function removeActiveAnswer() {
+  const activeAnswers = document.querySelectorAll(".activeAnswer");
+  activeAnswers.forEach((e) => e.classList.remove("activeAnswer"));
+}
+
+function enableDisableMarkBtn() {
+  markBtn.disabled = markedQuestions.has(currentQuestionIndex);
 }
