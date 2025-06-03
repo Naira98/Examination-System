afbcb2d6d35851fbf50319977fc58ec71b0195d1
Naira98 <nairamm99@gmail.com>
2025-06-03 23:42:11 +0300

    remove modules and use common js in login files

--- src/services/login.js
+++ src/services/login.js
@@ -1,7 +1,3 @@
-import { compareHashes } from "../utils/hashing.js";
-import { showHidePassword } from "../utils/showHidePassword.js";
-import { vaildateEmail, validatePassword } from "../utils/validation.js";
-
 const form = document.querySelector("form");
 const email = document.getElementById("email");
 const password = document.querySelector("#password");
@@ -10,66 +6,32 @@ const emailError = document.querySelector("#emailError");
 const passwordError = document.querySelector("#passwordError");
 const rememberMe = document.getElementById("rememberMe");
 
-// Show/hide password toggle setup
 showHidePassword([
   { button: showPassword, icon: showPassword.children[0], input: password },
 ]);
 
-// Prefill email if remembered
-document.addEventListener("DOMContentLoaded", () =>
-{
-  const rememberedEmail = localStorage.getItem("rememberedEmail");
-  if (rememberedEmail)
-  {
-    email.value = rememberedEmail;
-    rememberMe.checked = true;
-  }
-});
-
-// Form submission
-form.addEventListener("submit", (e) =>
-{
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   let isDataValid =
     vaildateEmail(email, emailError) &&
     validatePassword(password, passwordError);
 
-  console.log({ isDataValid });
-
-  if (isDataValid)
-  {
+  if (isDataValid) {
     const registeredUser = JSON.parse(localStorage.getItem("registeredUser"));
-
     if (
       email.value == registeredUser.email &&
-      compareHashes(password.value, registeredUser.password)
-    )
-    {
-      // Handle Remember Me checkbox
-      if (rememberMe.checked)
-      {
-        localStorage.setItem("rememberedEmail", email.value);
-      } else
-      {
-        localStorage.removeItem("rememberedEmail");
+      (await compareHashes(password.value, registeredUser.password))
+    ) {
+      if (rememberMe.checked) {
+        localStorage.setItem("rememberMe", true);
+      } else {
+        localStorage.removeItem("rememberMe");
       }
 
-      // Redirect to exam page
-      window.location.replace("/src/pages/exam.html");
-      return;
-    } else
-    {
+      window.location.replace("homePage.html");
+    } else {
       passwordError.innerText = "Invalid email or password.";
     }
   }
 });
-
-//   message.style.color = "green";
-//   message.textContent = "Login successful!";
-
-//   // Optional redirect simulation
-//   setTimeout(() => {
-//     document.body.innerHTML =
-//       "<h2 style='text-align:center;'>Welcome to the Exam Dashboard!</h2>";
-//   }, 1000);
